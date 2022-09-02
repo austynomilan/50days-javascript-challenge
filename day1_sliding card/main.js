a17967a5cc4dcd367c3a6af64d787f905c8cdfc9
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-02 06:25:35 +0100

    Add files via upload

--- day1_sliding card/main.js
+++ day1_sliding card/main.js
@@ -1,1 +1,48 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel =>{
+    panel.addEventListener('click', ()=>{
+        removeCLasses();
+        panel.classList.add('active');
+        
+    })
+})
+
+function removeCLasses(){
+panels.forEach(panel=>{
+  panel.classList.remove('active')
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
