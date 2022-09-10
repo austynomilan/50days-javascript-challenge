672d736d5017cc2c8f4706f70f59156d59f93640
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-11 00:51:53 +0100

    styles fixed and js written

--- day3_content styling/script.js
+++ day3_content styling/script.js
@@ -1,1 +1,14 @@
+let container = document.querySelector('.container')
 
+let close = document.getElementById('close')
+
+let open = document.getElementById('open')
+
+showNav = () =>{
+    if(container.classList.contains('show-nav')){
+        container.classList.remove('show-nav')
+    }else{container.classList.add('show-nav')}
+}
+
+open.addEventListener('click',showNav )
+close.addEventListener('click',showNav )
