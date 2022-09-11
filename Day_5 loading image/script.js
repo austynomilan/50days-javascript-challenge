3961c620e7c9bf9f6c72cc07bc75d22fcea5bfca
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-12 00:32:15 +0100

    initial

--- Day_5 loading image/script.js
+++ Day_5 loading image/script.js
@@ -1,1 +1,23 @@
+const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading_text')
+
+load = 0;
+
+let int = setInterval(blurring, 30)
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+function blurring(){
+    load++
+
+    if(load > 99){
+        clearInterval(int)
+    }
+   
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
 
