0abccd839b984278eeff3d362ccd326b58ac3f26
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-11 02:46:05 +0100

    initials

--- Day4_hidden search widget/script.js
+++ Day4_hidden search widget/script.js
@@ -1,1 +1,11 @@
+let search = document.querySelector('.search');
 
+let inputs = document.querySelector('.inputs');
+console.log(inputs)
+search.addEventListener('click', ()=>{
+    if(inputs.classList.contains('visible')){
+        inputs.classList.remove('visible')
+    }else{
+        inputs.classList.add('visible')
+    }
+})
