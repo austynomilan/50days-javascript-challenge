9c4b359eb76432c6ad3400e4e861e94f5c64cf31
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-12 13:12:33 +0100

    initial

--- Day6_scroll animation/main.js
+++ Day6_scroll animation/main.js
@@ -1,1 +1,56 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checker)
+
+checker()
+
+function checker() {
+boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        const triggerBottom = window.innerHeight / 5 * 4
+
+        if(boxTop < triggerBottom){
+            box.classList.add('show')
+        }else{
+            box.classList.remove('show')
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
