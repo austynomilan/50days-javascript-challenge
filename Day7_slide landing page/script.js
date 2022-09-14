f197cea126d55234a747aa8d2a183f557d809bce
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-14 14:20:56 +0100

    js added

--- Day7_slide landing page/script.js
+++ Day7_slide landing page/script.js
@@ -1,1 +1,23 @@
+const container = document.querySelector('.container');
 
+const left = document.querySelector('.left');
+
+
+const right = document.querySelector('.right');
+
+left.addEventListener('mouseenter', ()=>{
+    container.classList.add('hover-act1')
+})
+
+left.addEventListener('mouseleave', ()=>{
+    container.classList.remove('hover-act1')
+})
+
+right.addEventListener('mouseenter', ()=>{
+    container.classList.add('hover-act2')
+})
+
+
+right.addEventListener('mouseleave', ()=>{
+    container.classList.remove('hover-act2')
+})
