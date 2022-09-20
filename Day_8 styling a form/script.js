3eaa79cd1b2d4e4ec746ffcbe68044e1cb4f0272
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-20 06:01:17 +0100

    js added

--- Day_8 styling a form/script.js
+++ Day_8 styling a form/script.js
@@ -1,1 +1,8 @@
+const labels = document.querySelectorAll('.form_input label');
 
+labels.forEach(label=>{
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, index)=> `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+    .join('')
+})
