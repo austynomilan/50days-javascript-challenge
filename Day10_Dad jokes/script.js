2eaed01f22492af9c1cda826715ec33fc0b4d175
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-09-22 07:34:58 +0100

    Api integrated

--- Day10_Dad jokes/script.js
+++ Day10_Dad jokes/script.js
@@ -1,1 +1,21 @@
+const jokeContainer = document.getElementById('joke')
+const jokesChanger = document.getElementById('jokesBtn')
 
+generateJokes()
+
+jokesChanger.addEventListener('click', generateJokes)
+
+function generateJokes() {
+
+    const configuration = {
+        headers:{
+            Accept: 'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com', configuration )
+    .then((res) => res.json())
+    .then((data) => {
+        jokeContainer.innerHTML = data.joke
+    }
+    )}
