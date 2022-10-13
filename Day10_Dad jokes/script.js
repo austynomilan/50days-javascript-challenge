351d6af70a1c7592f4fcf22e28478cb14f956dcc
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-13 16:21:32 +0100

    applied promises

--- Day10_Dad jokes/script.js
+++ Day10_Dad jokes/script.js
@@ -5,7 +5,7 @@ generateJokes()
 
 jokesChanger.addEventListener('click', generateJokes)
 
-function generateJokes() {
+async function generateJokes() {
 
     const configuration = {
         headers:{
@@ -13,9 +13,15 @@ function generateJokes() {
         }
     }
 
-    fetch('https://icanhazdadjoke.com', configuration )
-    .then((res) => res.json())
-    .then((data) => {
-        jokeContainer.innerHTML = data.joke
+    let joker = await fetch('https://icanhazdadjoke.com', configuration )
+    let data = await joker.json()
+    jokeContainer.innerHTML = data.joke
+
+
+
+    // fetch('https://icanhazdadjoke.com', configuration )
+    // .then((res) => res.json())
+    // // .then((data) => {
+    //     jokeContainer.innerHTML = data.joke
+    // }
     }
-    )}
