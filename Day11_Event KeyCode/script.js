896bf5fbc87dd568baeae0cae64ccf223d2cfd25
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-15 09:24:21 +0100

    js completed

---
+++ Day11_Event KeyCode/script.js
@@ -0,0 +1,22 @@
+let insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e)=>{
+console.log(e)
+insert.innerHTML=`
+
+<div class="key">
+        ${e.key === ' '? 'space':e.key}
+            <small>event.key</small>
+        </div>
+
+        <div class="key">
+            ${e.keyCode}
+            <small>event.keyCode</small>
+        </div>
+
+        <div class="key">
+            ${e.code}
+            <small>event.code</small>
+        </div>
+`
+})
